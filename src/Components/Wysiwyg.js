e67a3715397f2b3d76fd6494f9445e6ee34c88b9
Michael W <mwerbo2@gmail.com>
2019-01-24 10:58:11 -0600

    Add TinyMCE editor

---
+++ src/Components/Wysiwyg.js
@@ -0,0 +1,24 @@
+import React, { Component }  from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+
+
+class Wysiwyg extends React.Component {
+    handleEditChange = (e) => {
+        console.log(`Content changed: ${e.target.getContent()}`);
+    }
+
+    render() {
+       return (
+            <Editor apiKey='2v70mtgk4kz045dkbblsshf5xoky86546vqb4bvj4h3oaqds' cloudChannel='stable'
+                initialValue="<p>Testing editor, initial value</p>"
+                init={{
+                    plugins: 'link image code',
+                    toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
+                }}
+                onChange={this.handleEditChange}
+            />
+        );
+    }
+}
+
+export default Wysiwyg;
