ef748d5f999ec5a73fc7bb798d0fd4ea09db01ba
Michael W <mwerbo2@gmail.com>
2019-01-26 22:34:51 -0600

    Add in form submit

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,16 +3,12 @@ import  Wysiwyg  from './Wysiwyg';
 
 
 class Main extends React.Component {
-    constructor(props){
-        super(props);
-    }
 
     render() {
         return (
             <div>
                 <Wysiwyg />
             </div>
-
         )
     }
 
