ad566d809f159147bdbec080eec11afd147fbcef
Michael Werbowsky <mwerbo2@users.noreply.github.com>
2019-02-28 22:27:24 -0800

    Weather Axios set up

--- src/Components/PreviewWeather.js
+++ src/Components/PreviewWeather.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import {Container, Header} from 'semantic-ui-react';
+import axios from 'axios';
 
 class Weather extends React.Component {
     state = {currentWeather: "Sunny and warm, 65˚"};
     
+    componentDidMount(){
+      axios.get('/weather')
+      .then(res => {
+        this.setState({ currentWeather: res.weather})})
+        .catch(err => {console.log(err);)  
+    }
+    
     render(){
         return(
             <Container>
-                <Header as="h1">{this.state.currentWeather}</Header>
+                <Header as="h1">{this.state.currentWeather.temp}</Header>
+                <Header as="h1">{this.state.currentWeather.conditions}</Header>
             </Container>
         )
     }
