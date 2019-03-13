3f10312ef9aa13f0646ae459c93c2a0ad0cac0c8
Michael W <mwerbo2@gmail.com>
2019-03-12 20:20:41 -0500

    Inline setup

--- src/Components/Announcement/PreviewAnnouncements.js
+++ src/Components/Announcement/PreviewAnnouncements.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Container, Header, Grid} from 'semantic-ui-react';
+import { Editor } from '@tinymce/tinymce-react';
 
 class Announcement extends React.Component {
     render() {
@@ -8,8 +9,28 @@ class Announcement extends React.Component {
                 <Grid.Row>
                     <Grid.Column>
                         <Container>
-                            <div dangerouslySetInnerHTML={{__html: this.props.title}}/>
-                            <div dangerouslySetInnerHTML={{__html: this.props.body}}/>
+                        <Editor inline apiKey='2v70mtgk4kz045dkbblsshf5xoky86546vqb4bvj4h3oaqds' tagName="div" plugins="link table wordcount" toolbar="bold link table" />
+                        <Editor
+                                apiKey='2v70mtgk4kz045dkbblsshf5xoky86546vqb4bvj4h3oaqds' 
+                                cloudChannel='stable'
+                                
+                                // selector='.title'
+                                // value={this.props.content} 
+                                // onEditorChange={this.handleEditorChange} 
+                                // initialValue="<p>Testing editor, initial value</p>"
+                                //tagName
+                                init={{
+                                    inline: true,
+                                    // tagname:'.title',
+                                    // selector: '.title',
+                                    // height: '500',
+                                    plugins: 'link image code',
+                                    toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
+                                }}
+                            //    onChange={this.handleEditChange}
+                            /> 
+                            <div className="title" dangerouslySetInnerHTML={{__html: this.props.title}}/>
+                            <div className="body" dangerouslySetInnerHTML={{__html: this.props.body}}/>
                         </Container>
                     </Grid.Column>
                 </Grid.Row>
