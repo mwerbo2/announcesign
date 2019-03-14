3e2c3df02bba19464baaf8e2044270f9460d398c
Michael W <mwerbo2@gmail.com>
2019-03-13 23:16:01 -0500

    Set up scheduling modal

--- src/Components/Announcement/PreviewAnnouncements.js
+++ src/Components/Announcement/PreviewAnnouncements.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container, Grid, Icon} from 'semantic-ui-react';
+import {Container, Grid, Icon, Modal, Button, Image, Header} from 'semantic-ui-react';
 import { Editor } from '@tinymce/tinymce-react';
 import axios from 'axios';
 
@@ -9,6 +9,7 @@ class Announcement extends React.Component {
         this.handleTitleChange = this.handleTitleChange.bind(this); 
         this.handleBodyChange = this.handleBodyChange.bind(this);
         this.saveAnnouncement = this.saveAnnouncement.bind(this);
+        this.scheduleAnnouncement = this.scheduleAnnouncement.bind(this);
 
         this.state = {
             title: "",
@@ -44,9 +45,22 @@ class Announcement extends React.Component {
     // this.props.deleteAnnouncement
     // }
 
-    // scheduleAnnouncement = () => {
-    //     console.log('schedule announcement');
-    // }
+    scheduleAnnouncement = () => {
+        console.log('modal open')
+        return (
+        <Modal trigger={<Icon name='calendar times outline' size='large'/>}>
+        <Modal.Header>Select a Photo</Modal.Header>
+        <Modal.Content image>
+          <Image wrapped size='medium' src='https://react.semantic-ui.com/images/avatar/large/rachel.png' />
+          <Modal.Description>
+            <Header>Default Profile Image</Header>
+            <p>We've found the following gravatar image associated with your e-mail address.</p>
+            <p>Is it okay to use this photo?</p>
+          </Modal.Description>
+        </Modal.Content>
+      </Modal>
+      )
+    }
 
     handleTitleChange(event) {
         this.setState({title: event})
@@ -76,7 +90,17 @@ class Announcement extends React.Component {
                         <Grid.Column floated="right" verticalAlign='middle'  width={2}>
                             <Icon name='trash alternate' size='large' onClick={this.props.onDelete}/>    
                             <Icon type="Submit" name='save' size='large'onClick={this.saveAnnouncement}/>
-                            <Icon name='calendar times outline' size='large' onClick={this.scheduleAnnouncement}/>
+                            <Modal trigger={<Icon name='calendar times outline' size='large'/>}>
+                                <Modal.Header>Select a Photo</Modal.Header>
+                                <Modal.Content image>
+                                <Image wrapped size='medium' src='https://react.semantic-ui.com/images/avatar/large/rachel.png' />
+                                <Modal.Description>
+                                    <Header>Default Profile Image</Header>
+                                    <p>We've found the following gravatar image associated with your e-mail address.</p>
+                                    <p>Is it okay to use this photo?</p>
+                                </Modal.Description>
+                                </Modal.Content>
+                            </Modal>
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
