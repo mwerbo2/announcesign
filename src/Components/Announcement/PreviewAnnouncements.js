9c1610a57d2320da65e2d909d925acd7df6e5960
Michael W <mwerbo2@gmail.com>
2019-03-13 19:48:50 -0500

    Grab tinymce content, axios post to save

--- src/Components/Announcement/PreviewAnnouncements.js
+++ src/Components/Announcement/PreviewAnnouncements.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {Container, Header, Grid, Icon, Form} from 'semantic-ui-react';
+import {Container, Grid, Icon} from 'semantic-ui-react';
 import { Editor } from '@tinymce/tinymce-react';
+import axios from 'axios';
 
 class Announcement extends React.Component {
     constructor(props){
         super(props);
-        // this.handleTitleChange = this.handleTitleChange.bind(this);
-        // this.handleBodyChange = this.handleBodyChange.bind(this);
-        // this.saveAnnouncement = this.saveAnnouncement.bind(this);
+        this.handleTitleChange = this.handleTitleChange.bind(this); 
+        this.handleBodyChange = this.handleBodyChange.bind(this);
+        this.saveAnnouncement = this.saveAnnouncement.bind(this);
 
         this.state = {
             title: "",
@@ -15,11 +16,21 @@ class Announcement extends React.Component {
         }
     }
 
-    // saveAnnouncement = (e) => {
-    //     // console.log("refs", this.refs.title)
-    //     // console.log('saving', e.target);
-    //     // console.log(tinymce.get('ID'))
-    // }
+    saveAnnouncement = (e) => {
+        axios.post('/announcements', {
+            user_id: 999999993,
+            announcement_title: this.state.title,
+            announcement_body: this.state.body
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+        
+
+    }
 
     // deleteAnnouncement = (e) => {
     //     console.log(e);
@@ -29,22 +40,22 @@ class Announcement extends React.Component {
     //     console.log('schedule announcement');
     // }
 
-    // handleTitleChange(event) {
-    //     this.setState({title: event})
-    //     console.log("Updating title to: ", this.state.title);
-    //   }
-    //   handleBodyChange(event) {
-    //     this.setState({body: event})
-    //     console.log("Updating body to: ", this.state.body);
-    //   }
+    handleTitleChange(event) {
+        this.setState({title: event})
+        console.log("Updating title to: ", this.state.title);
+      }
+      handleBodyChange(event) {
+        this.setState({body: event})
+        console.log("Updating body to: ", this.state.body);
+      }
     render() {
         return (
             <Grid>
                 <Grid.Row>
                     <Grid.Column width={14}>
                         <Container >
-                            <Editor id='title' inline apiKey='2v70mtgk4kz045dkbblsshf5xoky86546vqb4bvj4h3oaqds' initialValue={this.props.title} plugins="link table wordcount" toolbar="bold link table" onEditorChange={this.handleTitleChange} >
-                                <div  ref="title" className="title" dangerouslySetInnerHTML={{__html: this.props.title}}/>     
+                        <Editor ref="body" inline apiKey='2v70mtgk4kz045dkbblsshf5xoky86546vqb4bvj4h3oaqds' initialValue={this.props.title} plugins="link table wordcount" toolbar="bold link table" onEditorChange={this.handleTitleChange}>
+                                <div ref="title" name="title" className="title" dangerouslySetInnerHTML={{__html: this.props.title}}/>     
                             </Editor>
                             <Editor ref="body" inline apiKey='2v70mtgk4kz045dkbblsshf5xoky86546vqb4bvj4h3oaqds' initialValue={this.props.body} plugins="link table wordcount" toolbar="bold link table" onEditorChange={this.handleBodyChange}>
                                 <div name="body" className="body" dangerouslySetInnerHTML={{__html: this.props.body}}/>
