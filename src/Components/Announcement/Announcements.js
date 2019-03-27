d6144b508e135c096f7b744d8b8beca1cc5542c9
Michael W <mwerbo2@gmail.com>
2019-03-27 15:18:07 -0500

    Refactor announcements to be contained in a separate component

--- src/Components/Announcement/Announcements.js
+++ src/Components/Announcement/Announcements.js
@@ -1,1 +1,119 @@
 import React from 'react';
+import {Grid, Container, Header} from 'semantic-ui-react'
+import axios from 'axios';
+// import { Editor } from '@tinymce/tinymce-react';
+// import Weather from './Announcement/PreviewWeather';
+// import DateTime from './Announcement/PreviewDateTime';
+import Announcement from './Announcement';
+import AddButton from '../AddButton';
+import AnnouncementPlaceholder from './AnnouncementPlaceholder';
+import auth0Client from '../../Auth';
+
+
+
+class Announcements extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = { 
+            fullAnnouncement: [],
+            title: "true",
+            body: "true",
+            content: "",
+            live: true,
+            add: false, 
+            deleted: false
+        };
+        
+        this.handleEditorChange = this.handleEditorChange.bind(this);
+        // this.deleteAnnouncement = this.deleteAnnouncement.bind(this);
+        this.clickAdd = this.clickAdd.bind(this);
+        // this.handleEditChange = this.handleEditChange.bind(this);
+        this.getCurrentPosts = this.getCurrentPosts.bind(this)
+    }
+    //RAISED TO PARENTS
+    // handleEditorChange(content) {
+    //     this.props.onEditorChange(content)
+    // }
+
+    handleSubmit = (e) => {
+       
+    }
+    
+    clickAdd = () => {
+        this.setState({ 
+            add: true,
+            selected: true
+        });
+    }
+
+    handleEditorChange(content) {
+        this.setState({ content });
+        console.log(this.state.content)
+      }
+
+      getCurrentPosts = () => {
+        console.log('Line 66 A.js getting current post')
+        axios.get('/announcements/status/active')
+        .then(announcement => {
+            this.setState({
+                fullAnnouncement: announcement.data,
+                title: announcement.title,
+                body: announcement.body
+            })
+        })
+        .catch(error => console.log(error))
+    }
+    
+    deleteAnnouncement() {
+        console.log(this.state)
+        axios.post('/announcements/status', {
+            user_id: 999992,
+            id: this.post_id,
+            status: 'archive'
+        })
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+    }
+
+
+    componentDidMount(){
+        this.getCurrentPosts();
+    }
+
+    render(){
+        if (!auth0Client.isAuthenticated()) return (
+            <Container key={this.props.key} style={{padding: '3em 0em 0em' }}>
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column width={16}>
+                            {this.state.fullAnnouncement.map(announce => {
+                                return <Announcement key={announce.id} isLive={this.state.live} onDelete={this.deleteAnnouncement} onEditorChange={this.handleEditorChange} post_id={announce.id} title={announce.announcement_title} body={announce.announcement_body} />    
+                            })}
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Container>
+        )
+        return (
+            <Container key={this.props.key} style={{padding: '3em 0em 0em' }}>
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column width={16}>
+                                {this.state.fullAnnouncement.map(announce => {
+                                    return <Announcement key={announce.id} isLive={this.state.live} onDelete={this.deleteAnnouncement} onEditorChange={this.handleEditorChange} post_id={announce.id} title={announce.announcement_title} body={announce.announcement_body} /> 
+                                })}
+                                <AddButton buttonClick={this.clickAdd} />
+                                {
+                                    this.state.add &&
+                                    <AnnouncementPlaceholder /> 
+                                }
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Container>
+        )
+    }
+}
+
+export default Announcements;
--- src/Components/DisplayEditor.js
+++ src/Components/DisplayEditor.js
@@ -4,9 +4,10 @@ import axios from 'axios';
 // import { Editor } from '@tinymce/tinymce-react';
 import Weather from './Announcement/PreviewWeather';
 import DateTime from './Announcement/PreviewDateTime';
-import Announcement from './Announcement/PreviewAnnouncements';
+import Announcement from './Announcement/Announcement';
 import AddButton from './AddButton';
 import AnnouncementPlaceholder from './Announcement/AnnouncementPlaceholder';
+import Announcements from './Announcement/Announcements';
 import auth0Client from '../Auth';
 
 
@@ -24,9 +25,9 @@ class Display extends React.Component {
             add: false
         };
         
-        this.handleEditorChange = this.handleEditorChange.bind(this);
+        // this.handleEditorChange = this.handleEditorChange.bind(this);
         // this.deleteAnnouncement = this.deleteAnnouncement.bind(this);
-        this.clickAdd = this.clickAdd.bind(this);
+        // this.clickAdd = this.clickAdd.bind(this);
         // this.handleEditChange = this.handleEditChange.bind(this);
     }
     //RAISED TO PARENTS
@@ -34,33 +35,33 @@ class Display extends React.Component {
     //     this.props.onEditorChange(content)
     // }
 
-    handleSubmit = (e) => {
+    // handleSubmit = (e) => {
        
-    }
+    // }
     
-    clickAdd = () => {
-        this.setState({ 
-            add: true,
-            selected: true
-        });
-    }
+    // clickAdd = () => {
+    //     this.setState({ 
+    //         add: true,
+    //         selected: true
+    //     });
+    // }
 
-    handleEditorChange(content) {
-        this.setState({ content });
-        console.log(this.state.content)
-      }
+    // handleEditorChange(content) {
+    //     this.setState({ content });
+    //     console.log(this.state.content)
+    //   }
     
-    deleteAnnouncement = (e) => {
-        console.log("line 54 de.js ", this)
-        console.log("Ref: ", this.refs)
-        console.log("Target: ", e.target.parentNode.parentNode)
-        console.log("Target: ", e.target.parentNode.parentNode.value)
-        // this.setState({ live: false });
-        console.log(this.state.live)
-        // axios.post({
+    // deleteAnnouncement = (e) => {
+    //     console.log("line 54 de.js ", this)
+    //     console.log("Ref: ", this.refs)
+    //     console.log("Target: ", e.target.parentNode.parentNode)
+    //     console.log("Target: ", e.target.parentNode.parentNode.value)
+    //     // this.setState({ live: false });
+    //     console.log(this.state.live)
+    //     // axios.post({
 
-        // })
-    }
+    //     // })
+    // }
 
     // showLiveAnnouncements = () => {
     //     this.state.fullAnnouncement.map(announce => {
@@ -68,59 +69,20 @@ class Display extends React.Component {
     //         })
     // }
 
-    componentDidMount(){
-        console.log('getting active announcements')
-        axios.get('/announcements/status/active')
-
-        .then(announcement => {
-            console.log(announcement.data)
-            this.setState({
-                fullAnnouncement: announcement.data,
-                title: announcement.title,
-                body: announcement.body
-            })
-            // console.log(this.state.fullAnnouncement);
-        })
-        .catch(error => console.log(error))
-    }
-
-
+    // componentDidMount(){
+    //     console.log('Line 73 DE.js, getting active announcements')
+    //     axios.get('/announcements/status/active')
+    //     .then(announcement => {
+    //         this.setState({
+    //             fullAnnouncement: announcement.data,
+    //             title: announcement.title,
+    //             body: announcement.body
+    //         })
+    //     })
+    //     .catch(error => console.log(error))
+    // }
 
     render(){
-        if (!auth0Client.isAuthenticated()) return (
-            <Container key={this.props.key} style={{padding: '3em 0em 0em' }}>
-            <Grid>
-                <Grid.Row>
-                    <Grid.Column width={4}>
-                        <Weather />
-                    </Grid.Column>
-                    <Grid.Column width={8}>
-                        <Header textAlign='center' as="h1">Announcements</Header>
-                    </Grid.Column>
-                    <Grid.Column width={4}>
-                        <DateTime />
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column width={16}>
-                            {this.state.fullAnnouncement.map(announce => {
-                                // if (this.state.live) {
-                                    return <Announcement key={announce.id} isLive={this.state.live} onDelete={this.deleteAnnouncement} onEditorChange={this.handleEditorChange} post_id={announce.id} title={announce.announcement_title} body={announce.announcement_body} /> 
-                                // } else {
-                                    return ""
-                                // }
-                            })}
-                        
-                        {
-                            this.state.add &&
-                            <AnnouncementPlaceholder /> 
-                        }
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        </Container>
-
-        )
         return (
             <Container key={this.props.key} style={{padding: '3em 0em 0em' }}>
                 <Grid>
@@ -136,23 +98,10 @@ class Display extends React.Component {
                         </Grid.Column>
                     </Grid.Row>
                     <Grid.Row>
-                        <Grid.Column width={16}>
-                                {this.state.fullAnnouncement.map(announce => {
-                                    // if (this.state.live) {
-                                        return <Announcement key={announce.id} isLive={this.state.live} onDelete={this.deleteAnnouncement} onEditorChange={this.handleEditorChange} post_id={announce.id} title={announce.announcement_title} body={announce.announcement_body} /> 
-                                    // } else {
-                                        return ""
-                                    // }
-                                })}
-                            <AddButton buttonClick={this.clickAdd} />
-                            {
-                                this.state.add &&
-                                <AnnouncementPlaceholder /> 
-                            }
-                        </Grid.Column>
+                        <Announcements />
                     </Grid.Row>
                 </Grid>
-            </Container>
+        </Container>
         )
     }
 }
