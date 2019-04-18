566d7f5933f2f5ff007f5f0967b21c2f64dccac6
Michael W <mwerbo2@gmail.com>
2019-04-18 13:27:59 -0500

    Modal state passed

--- src/Components/Announcement/DateTimePicker.js
+++ src/Components/Announcement/DateTimePicker.js
@@ -40,41 +40,6 @@ class DateAndTimePickers extends React.Component {
     };
   }
 
-
-  // showScheduled = (response) => {
-  //   console.log('hellw show')
-  //   if(!response.data) {
-  //     return (
-      
-  //     )
-  //   } else {
-  //       return ( 
-  //       <List divided relaxed>
-  //         <List.Item>
-  //           <List.Icon name='github' size='large' verticalAlign='middle' />
-  //           <List.Content>
-  //             <List.Header as='a'>Semantic-Org/Semantic-UI</List.Header>
-  //             <List.Description as='a'>Updated 10 mins ago</List.Description>
-  //           </List.Content>
-  //         </List.Item>
-  //         <List.Item>
-  //           <List.Icon name='github' size='large' verticalAlign='middle' />
-  //           <List.Content>
-  //             <List.Header as='a'>Semantic-Org/Semantic-UI-Docs</List.Header>
-  //             <List.Description as='a'>Updated 22 mins ago</List.Description>
-  //           </List.Content>
-  //         </List.Item>
-  //         <List.Item>
-  //           <List.Icon name='github' size='large' verticalAlign='middle' />
-  //           <List.Content>
-  //             <List.Header as='a'>Semantic-Org/Semantic-UI-Meteor</List.Header>
-  //             <List.Description as='a'>Updated 34 mins ago</List.Description>
-  //           </List.Content>
-  //         </List.Item>
-  //       </List>
-  //       )
-  //   }
-  // }
   componentDidMount() {
     console.log("mount: ", this.props.post_id)
     axios.get(`/schedules/${this.props.post_id}`)
